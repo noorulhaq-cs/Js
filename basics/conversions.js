9573198ec5cfa4f7acf4e9861fbf8707cb7599ab
Noor ul Haq <157638741+noorulhaq-cs@users.noreply.github.com>
2025-07-29 12:36:08 +0000

    Add conversions.js with initial variable type conversions.

---
+++ basics/conversions.js
@@ -0,0 +1,33 @@
+let score = "Noor"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// when changed into Number
+// "33" => 33
+// "33abc" => NaN
+//  true => 1
+// false => 0
+
+let isLoggedIn = "Noor"
+
+console.log(typeof isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1,12,-45 => true
+// 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
