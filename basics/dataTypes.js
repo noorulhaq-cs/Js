f2509fd036cdde786f2a461622217ba7908cf542
Noor ul Haq <157638741+noorulhaq-cs@users.noreply.github.com>
2025-07-29 11:20:07 +0000

    variables and dataTypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,24 @@
+
+let name = "Noor"
+let age = 22
+let isAdult = true
+let status = null
+let salary;
+
+// BigInt: is use for vary big number
+
+console.log(name)
+console.log(age)
+console.log(isAdult)
+console.log(status)
+console.log(salary)
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof isAdult)
+console.log(typeof status)
+console.log(typeof null) // null is a object, spacial data type
+console.log(typeof salary)
+
+// these are all premitive dataTypes
+// object is non premitive datatype
---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+
+const stdId = 12345
+let stdName = "Noor ul Haq"
+
+// var ham ab use nahi karte hain scope ki vaja se, ye purana tareqa h.
+
+console.log(stdId)
+console.log(stdName)
+
+console.table([stdId,stdName])
